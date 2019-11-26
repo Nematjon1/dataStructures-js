8a0bdbdb081a42c25cc5bb5b022686fed82aa0ba
Nematjon <frank.sport.93@gmail.com>
2019-11-26 16:41:50 +0300

    Provided some examples

--- array-ds.js
+++ array-ds.js
@@ -59,3 +59,58 @@ class MyArray {
     this.length--;
   }
 }
+
+/* Create a function that reverses a string:
+ * "Hi My name is Nematjon" should be:
+ * "nojtameN si eman yM iH"
+ */
+function reverseString(string) {
+  // Check an input
+  if(!string || typeof string !== "string" || string.length < 2) {
+    return "The input must be type of 'string' and greater than 1";
+  }
+  // string = ["s", "t", "r"];
+  let reversed = [];
+  const totalItems = string.length - 1;
+  for(let i = totalItems; i >= 0; i--) {
+    reversed.push(string[i]);
+  }
+  return reversed.join("");
+}
+//console.log(reverseString("string"));
+//console.log(reverseString(undefined));
+
+function reverseString2(string){
+  return string.split("").reverse().join("");
+}
+const reverseString3 = string => [...string].reverse().join("");
+//console.log(reverseString3("Hi My name is SSSS"))
+
+/* Merge 2 sorted arrays */
+function mergeSortedArray(arr1, arr2) {
+  let mergedArray = [];
+  // Check inputs
+  if(arr1.length === 0) {
+    return arr2;
+  } else if(arr2.length === 0) {
+    return arr1;
+  }
+  let arr1Item = arr1[0];
+  let arr2Item = arr2[0];
+  let i = 0;
+  let j = 0;
+  while(arr1Item || arr2Item) {
+    console.log(arr1Item, arr2Item);
+    if(!arr2Item || arr1Item < arr2Item) {
+      mergedArray.push(arr1Item);
+      arr1Item = arr1[i];
+      i++;
+    } else {
+      mergedArray.push(arr2Item);
+      arr2Item = arr2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
